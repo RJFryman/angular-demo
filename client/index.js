15b10951b07496b98da13c42603101da80b32ec5
Robert Fryman <robert.fryman@gmail.com>
2014-09-08 13:59:02 -0500

    wrap up for demo

--- client/index.js
+++ client/index.js
@@ -4,5 +4,12 @@
   angular.module('demo', [])
   .controller('MainController', ['$scope', function($scope){
     $scope.name = 'RJFryman';
+    $scope.age = 23;
+    $scope.isBirthday;
+    $scope.dogs = [
+      {name:'fido', age:3},
+      {name:'baxter', age:2},
+      {name:'skip', age:6}
+    ];
   }]);
 })();
